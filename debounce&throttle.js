ea3883c094e36be435d3895526d91074b7a4e883
lhdai <d_muses@163.com>
2024-02-27 17:22:21 +0800

    update

--- debounce&throttle.js
+++ debounce&throttle.js
@@ -38,4 +38,4 @@ function showTop  () {
   var scrollTop = document.body.scrollTop || document.documentElement.scrollTop
   console.log('滚动条位置：' + scrollTop)
 }
-window.onscroll = throttle(showTop, 1000) 
+window.onscroll = throttle(showTop, 1000)
