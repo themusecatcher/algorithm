fbdc0739f12883187e00d77e6089c14c793a3f2c
themusecatcher <themusecatcher@163.com>
2025-05-12 12:24:31 +0800

    update myBind

--- myBind.js
+++ myBind.js
@@ -17,20 +17,33 @@ function getName (a, b) {
   args 两种获取方式：
   1. 直接在函数形参中使用 剩余参数语法（...args）进行收集
   2. 使用 arguments 对象，并通过 Array.prototype.slice 方法截取除去第一个参数外的其余参数
+    let args = [...arguments] // arguments：对应于传递给函数的参数的类数组对象
+    args = args.slice(1) // 截取除第一个参数 context 外的其余参数
 */
 Function.prototype.myBind = function (context, ...args) {
-  const fn = this
-  // let args = [...arguments] // arguments：对应于传递给函数的参数的类数组对象
-  // args = args.slice(1) // 截取除第一个参数 context 外的其余参数
+  return () => {
+    return this.call(context, ...args)
+  }
+}
+Function.prototype.myBind = function (context, ...args) {
+  /*
+    普通函数中的 this 指向取决于函数是如何调用的，即运行时确定 this 指向
+    因为是通过 getName.myBind 调用的，因此 this 指向(就是) getName
+  */
+  const fn = this // [Function: getName]
   return function () {
     return fn.call(context, ...args)
   }
+  // 或者 直接使用箭头函数
+  // return () => {
+  //   return this.call(context, ...args)
+  // }
 }
 var player = {
   name: 'curry'
 }
 var myBindName = getName.myBind(player, '&', '30') // 将第一个参数 player 作为函数的 this 上下文，其余参数作为函数的入参
-console.log(myBindName) // f () {...}
+console.log(myBindName) // [Function (anonymous)]
 console.log('myBindName:', myBindName()) // curry&30
 
 var bindName = getName.bind(player, '&', '30')
