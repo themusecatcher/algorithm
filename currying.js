ab01d650f864f7aecae63f76d3e11d26eec12adf
themusecatcher <themusecatcher@163.com>
2025-05-12 09:46:39 +0800

    update currying

--- currying.js
+++ currying.js
@@ -1,28 +1,27 @@
-function  getLink (a, b) {
+function getLink (a, b) {
   return a + b
 }
 // 通用柯里化工具函数
-function currying (fn, args) {
-  args = args || [] // 柯里化时传入的参数
-  console.log('out-args:', args)
-  return function () {
-    // arguments是柯里化后传入的参数
-    console.log('in-args:', args) // 利用闭包把柯里化时的传参保存下来
-    let allArgs = [...args, ...arguments] // 合并参数
-    console.log('arguments:', [...arguments])
-    console.log('allArgs:', allArgs)
-    // 如果参数个数小于fn.length，则递归调用，继续收集参数
-    if (allArgs.length < fn.length) { // fn.length：fn接收的形参个数
-      // return currying.call(this, fn, ...allArgs) // call接收参数列表
-      return currying(fn, allArgs)
+function currying (fn, ...args) {
+  return function (...rest) {
+    // 拼接参数
+    const allArgs = args.concat(rest) // 或 allArgs = [...args, ...rest]
+    // 参数个数大于等于 fn.length 时，参数收集完毕，执行函数
+    if (allArgs.length >= fn.length) { // fn.length：fn ，接收的形参个数
+      return fn.apply(this, allArgs)
     }
-    // 参数收集完，则执行fn
-    // return fn.call(this, ...allArgs) // apply接收参数数组
-    return fn(...allArgs)
+    return currying(fn, ...allArgs)
   }
 }
-var curryingLink = currying(getLink)
-console.log('curryingLink:', curryingLink) // function {...}
-console.log('(a,b):', curryingLink('a', 'b')) // ab
-console.log('(a):', curryingLink('a')) // function {...}
-console.log('(a)(b):', curryingLink('a')('b')) // ab
+const curryingLink = currying(getLink)
+console.log(curryingLink) // [Function (anonymous)] function {...}
+console.log(curryingLink('a', 'b')) // ab
+console.log(curryingLink('a')) // [Function (anonymous)] function {...}
+console.log(curryingLink('a')('b')) // ab
+
+const sum = (a, b, c, d) => a + b + c + d
+
+console.log(currying(sum)(1)(2)(3)(4)) // 10
+console.log(currying(sum, 1)(2, 3)(4)) // 10
+console.log(currying(sum, 1, 2)(3)(4)) // 10
+console.log(currying(sum, 1, 2)(3, 4)) // 10
