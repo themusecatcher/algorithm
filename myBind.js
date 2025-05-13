c4bacfacb07d4f6e8deaf3ae48ecaade71ed9477
themusecatcher <themusecatcher@163.com>
2025-05-13 11:34:51 +0800

    update myBind

--- myBind.js
+++ myBind.js
@@ -1,18 +1,6 @@
 function getName (a, b) {
   return this.name + a + b
 }
-// Function.prototype.myBind = function (context) {
-//   const fn = this
-//   // 将 arguments 对象转为数组
-//   // for (const arg of arguments) {
-//   //   console.log('arg', arg)
-//   // }
-//   let args = [...arguments] // arguments：对应于传递给函数的参数的类数组对象
-//   args = args.slice(1) // 截取除第一个参数 context 外的其余参数
-//   return function () {
-//     return fn.apply(context, args) // apply 第二个参数接收一个参数数组
-//   }
-// }
 /*
   args 两种获取方式：
   1. 直接在函数形参中使用 剩余参数语法（...args）进行收集
