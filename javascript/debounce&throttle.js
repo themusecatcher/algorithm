a98fa973993685b6cfea7b8a94fbdab09cb5526c
themusecatcher <themusecatcher@163.com>
2025-11-03 11:43:53 +0800

    feat: update algorithm

--- javascript/debounce&throttle.js
+++ javascript/debounce&throttle.js
@@ -2,13 +2,18 @@
   防抖（debounce）：对于短时间内连续触发的事件（上面的滚动事件），
   防抖就是让某个时间期限（如上面的1000毫秒）内，事件处理函数只执行一次。
 */
-function debounce (fn, delay){
-  let timer = null //借助闭包
-  return function () {
+function debounce(fn: Function, delay: number = 300): Function {
+  let timer: any = null // 使用闭包保存定时器的引用
+  return function (...args: any[]) {
+    // 返回一个包装函数
     if (timer) {
+      // 如果定时器存在，则清除之前的定时器
       clearTimeout(timer)
     }
-    timer = setTimeout(fn, delay)
+    // 设置新的定时器，延迟执行原函数
+    timer = setTimeout(() => {
+      fn(...args)
+    }, delay)
   }
 }
 function showTop  () {
@@ -21,17 +26,18 @@ window.onscroll = debounce(showTop, 300)
   节流（throttle）：如果短时间内大量触发同一事件，那么在函数执行一次之后，
   该函数在指定的时间期限内不再工作，直至过了这段时间才重新生效。
 */
-var throttle = function (fn, delay) {
-  var valid = true
-  return function () {
+function throttle(fn: Function, delay: number = 300): Function {
+  let valid = true // 用于标记函数是否可以执行
+  return function (...args: any[]) {
+    if (!valid) return false // 返回 false，表示当前不执行函数
+    // 返回一个新的函数，该函数负责执行节流逻辑
     if (valid) {
+      fn(...args) // 执行原函数
       valid = false // 将函数置为无效
       setTimeout(() => {
-        fn()
         valid = true
       }, delay)
     }
-    return false // valid为false时，函数不执行
   }
 }
 function showTop  () {
