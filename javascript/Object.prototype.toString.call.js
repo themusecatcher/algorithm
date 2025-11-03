350a9032b002f46ddb8803c79db5c6d1cf69a621
themusecatcher <themusecatcher@163.com>
2025-11-03 16:42:14 +0800

    feat: update algorithm

--- javascript/Object.prototype.toString.call.js
+++ javascript/Object.prototype.toString.call.js
@@ -1,17 +1,19 @@
-const obj = new Object() // 等价于 const obj = {}
-const arr = new Array() // 等价于 const arr = []
-const fn = function () {}
-const date = new Date()
-const reg = new RegExp('at', 'g')
-const newTru = new Boolean(true) // typeof：oject
-const newNum = new Number(3) // typeof：object
-const newStr = new String('world') // typeof：object
-const sym = Symbol('sy') // 表示独一无二的值，常用来定义对象的唯一属性名
-const tru = true // typeof：boolean
-const num = 5 // typeof：number
-const str = 'world' // typeof：string
-// var formData = new FormData()
-// formData.set('data', 1)
+const obj = new Object() // 等价于 const obj = {} typeof: oject
+const arr = new Array() // 等价于 const arr = [] typeof: oject
+const fn = function () {} // typeof: function
+const date = new Date() // typeof: oject
+const reg = new RegExp('at', 'g') // typeof: oject
+const newTru = new Boolean(true) // typeof: oject
+const newNum = new Number(3) // typeof: object
+const newStr = new String('world') // typeof: object
+const sym = Symbol('sy') // typeof: symbol 表示独一无二的值，常用来定义对象的唯一属性名
+const tru = true // typeof: boolean
+const num = 5 // typeof: number
+const str = 'world' // typeof: string
+const nul = null // typeof: object
+const undef = undefined // typeof: undefined
+const formData = new FormData() // typeof: object
+formData.set('data', 1)
 console.log('type obj:', Object.prototype.toString.call(obj)) // [object Object]
 console.log('type arr:', Object.prototype.toString.call(arr)) // [object Array]
 console.log('type fn:', Object.prototype.toString.call(fn)) // [object Function]
@@ -20,11 +22,12 @@ console.log('type reg:', Object.prototype.toString.call(reg)) // [object RegExp]
 console.log('type newTru:', Object.prototype.toString.call(newTru)) // [object Boolean]
 console.log('type newNum:', Object.prototype.toString.call(newNum)) // [object Number]
 console.log('type newStr:', Object.prototype.toString.call(newStr)) // [object String]
-console.log('type sym:', typeof sym) // symbol
 console.log('type sym:', Object.prototype.toString.call(sym)) // [object Symbol]
 console.log('type tru:', Object.prototype.toString.call(tru)) // [object Boolean]
 console.log('type num:', Object.prototype.toString.call(num)) // [object Number]
 console.log('type str:', Object.prototype.toString.call(str)) // [object String]
-// console.log('type formData:', Object.prototype.toString.call(formData)) // [object FormData]
+console.log('type nul:', Object.prototype.toString.call(nul)) // [object Null]
+console.log('type undef:', Object.prototype.toString.call(undef)) // [object Undefined]
+console.log('type formData:', Object.prototype.toString.call(formData)) // [object FormData]
 // console.log('type file:', Object.prototype.toString.call(file)) // [object File] 二进制文件流（binary）
-// 使用apply也一样
+// 使用 apply 也一样
