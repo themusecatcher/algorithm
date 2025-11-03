d28a89fb3b184ffa4fcc8023312706498edcf5a6
themusecatcher <themusecatcher@163.com>
2025-11-03 17:42:24 +0800

    feat: update sort

--- javascript/sort.js
+++ javascript/sort.js
@@ -7,7 +7,7 @@ function compareNum (a, b) {
   // 以对象的某一属性值进行降序
   return b.number - a.number
 }
-var sortList = [5, 0, 1, 10, -5]
+const sortList = [5, 0, 1, 10, -5]
 console.log('sort:', sortList.sort())
 console.log('compare sort:', sortList.sort(compare))
 
@@ -16,60 +16,51 @@ function bubbleSort (list) {
 	var len = list.length
 	for (var i = 0; i < len; i++) {
 		for (var j = i + 1; j < len; j++) {
-			if (list[i] > list[j]) {
-				const temp = list[i]
-				list[i] = list[j]
-				list[j] = temp
+			if (list[j] < list[i]) {
+				[list[i], list[j]] = [list[j], list[i]]
 			}
 		}
 	}
 	return list
 }
-var bubbleList = [-5, 0, 1, 10, 5]
+const bubbleList = [-5, 0, 1, 10, 5]
 console.log('bubbleSort:', bubbleSort(bubbleList))
 
-//选择排序
-var selectList = [5, 0, 1, 10, -5]
-// function selectSort (list) {
-// 	var len = list.length
-// 	var minIndex
-// 	for (var i = 0; i < len-1; i++) {
-// 		minIndex = i
-// 		for (var j = i + 1; j < len; j++) {
-// 			if (list[minIndex] > list[j]) {
-// 				minIndex = j
-// 			}
-// 		}
-//     if (minIndex !== i) {
-//       const temp = list[i]
-//       list[i] = list[minIndex]
-//       list[minIndex] = temp
-//     }
-// 	}
-// 	return list
-// }
-
-// 选择排序
-function selectSort (arr) {
-  const len = arr.length
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]]
-      }
+/**
+ * 选择排序
+ * 
+ * 每次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，直到全部待排序的数据元素排完
+ */
+const selectList = [5, 0, 1, 10, -5]
+function selectSort (list) {
+	const len = list.length
+	let minIndex = i
+	for (let i = 0; i < len - 1; i++) {
+		minIndex = i
+		for (let j = i + 1; j < len; j++) {
+			if (list[j] < list[minIndex]) {
+				minIndex = j
+			}
+		}
+    if (minIndex !== i) {
+      [list[i], list[minIndex]] = [list[minIndex], list[i]]
     }
-  }
-  return arr
+	}
+	return list
 }
 
 console.log('selectSort:', selectSort(selectList))
 
-// 插入排序
-var insertList = [5, 0, 1, 10, -5]
+/**
+ * 插入排序
+ * 
+ * 构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+ */
+const insertList = [5, 0, 1, 10, -5]
 function insertSort (list) {
-	var len = list.length
-	var preIndex, current
-	for (var i = 1; i < len; i++) {
+	const len = list.length
+	let preIndex, current
+	for (let i = 1; i < len; i++) {
 		preIndex = i - 1
 		current = list[i]
 		while (preIndex >= 0 && list[preIndex] > current) {
@@ -82,5 +73,5 @@ function insertSort (list) {
 }
 console.log('insertSort:', insertSort(insertList))
 
-var nums = [1, 2, [3, [4]], 5]
+const nums = [1, 2, [3, [4]], 5]
 console.log('flat-nums:', nums.flat())
